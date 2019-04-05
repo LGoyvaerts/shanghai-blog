0cacc0b5c62cf6b30f856ca0294fbb41b532adb6
gol <lorris.goyvaerts@ti8m.ch>
2019-04-05 11:40:19 +0200

    Initial Commit

---
+++ src/main/kotlin/BlogApplication.kt
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication{
+
+    fun main (args:Array<String>){
+        runApplication<BlogApplication>(*args)
+    }
+}
